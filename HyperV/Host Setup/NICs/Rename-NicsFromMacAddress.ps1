9b69a1812cf672bd5ebb3cc1913d496c70cbe8d5
Ab3r0n <RStieber@users.noreply.github.com>
2025-06-12 10:45:55 -0500

    Add files via upload
    Adding scripts to help with host setup

---
+++ HyperV/Host Setup/NICs/Rename-NicsFromMacAddress.ps1
@@ -0,0 +1,18 @@
+﻿$csvLocation = (Read-Host "Please enter the full path to the CSV with NIC Names and MAC Addresses").TrimStart("`"")
+
+$csv = Import-CSV $csvLocation
+$netAdapters = Get-NetAdapter
+
+foreach ($line in $csv) {
+    $mac = ($line.MacAddress).ToUpper().Replace(":","-")
+    $currentAdapter = $null
+    $currentAdapter = $netAdapters | Where {$_.MacAddress -eq $mac}
+
+    if($currentAdapter) {
+        Write-Host -ForegroundColor Cyan "Renaming Net Adapter '$($currentAdapter.Name)' ($($mac)) to '$($line.NewNicName)'"
+        $null = $currentAdapter | Rename-NetAdapter -PassThru -NewName $line.NewNicName
+        }
+    else {
+        Write-Host -ForegroundColor Yellow "No Net Adapter found with MAC address '$mac', skipping"
+    }
+}
